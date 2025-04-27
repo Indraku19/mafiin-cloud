8d23dd4d4f6d313f999885bf470d791fbed1da92
Indraku19 <115960583+Indraku19@users.noreply.github.com>
2025-04-27 20:27:42 +0700

    Update ScrollUp.tsx

--- src/components/Common/ScrollUp.tsx
+++ src/components/Common/ScrollUp.tsx
@@ -1,9 +1,22 @@
 "use client";
 
 import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 export default function ScrollUp() {
-  useEffect(() => window.document.scrollingElement?.scrollTo(0, 0), []);
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = () => {
+      window.scrollTo(0, 0);
+    };
+    
+    router.events.on('routeChangeComplete', handleRouteChange);
+    
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router]);
 
   return null;
 }
